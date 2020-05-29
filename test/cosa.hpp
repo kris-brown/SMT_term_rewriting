90beb823658e9646a18663cf817bc888dbca8495
Kris <ksb@stanford.edu>
2020-05-28 17:22:26 -0700

    Cosa example test with input variables

--- test/cosa.hpp
+++ test/cosa.hpp
@@ -16,6 +16,55 @@
 #include "utils/logger.h"
 #include "utils/logger.h"
 
+
+TEST_CASE("transition2") {
+  smt::SmtSolver s = smt::CVC4SolverFactory::create(false);
+
+  s->set_opt("produce-models", "true");
+  s->set_opt("incremental", "true");
+  cosa::FunctionalTransitionSystem fts(s);
+
+  smt::Sort Int = s->make_sort(smt::INT);
+  smt::Term state = fts.make_state("cnt", Int);
+  smt::Term inp0 = fts.make_input("i0", Int);
+  smt::Term inp1 = fts.make_input("i1", Int);
+  smt::Term zero = s->make_term(0, Int);
+  smt::Term one = s->make_term(1, Int);
+  smt::Term two = s->make_term(2, Int);
+  smt::Term n1 = s->make_term(-1, Int);
+  smt::Term n3 = s->make_term(-3, Int);
+
+  smt::Term i00 = s->make_term(smt::Equal, inp0, zero);
+  smt::Term i01 = s->make_term(smt::Equal, inp0, one);
+  smt::Term i11 = s->make_term(smt::Equal, inp1, one);
+  smt::Term i1n1 = s->make_term(smt::Equal, inp1, n1);
+  smt::Term i0const = s->make_term(smt::Or,i00,i01);
+  smt::Term i1const = s->make_term(smt::Or,i11,i1n1);
+  smt::Term i0i1 = s->make_term(smt::Mult, inp0, inp1);
+  smt::Term state1 = s->make_term(smt::Plus, state, one);
+
+  fts.constrain_init(s->make_term(smt::Equal, state, zero));
+  fts.constrain_inputs(s->make_term(smt::And,i0const,i1const));
+  fts.assign_next(state, s->make_term(smt::Mult, state1, i0i1));
+  smt::Term p = s->make_term(
+    smt::Not, s->make_term(smt::Equal,state, n3));
+  cosa::Property prop(fts, p);
+  cosa::Bmc bmc(prop, s);
+  cosa::ProverResult pr = bmc.check_until(5);
+  CHECK(pr == cosa::FALSE);
+
+  std::vector<smt::UnorderedTermMap> wit;
+  bmc.witness(wit);
+
+  std::vector<smt::UnorderedTermMap> expected{
+    {{state, zero},{inp1,one},{inp0,one}},
+    {{state, one},{inp1,one},{inp0,one}},
+    {{state, two},{inp1,n1},{inp0,one}},
+    {{state, n3},{inp1,zero},{inp0,zero}}};
+
+  CHECK(wit == expected);
+}
+
 TEST_CASE("transition system") {
 
   smt::SmtSolver s = smt::CVC4SolverFactory::create(false);
@@ -37,19 +86,27 @@ TEST_CASE("transition system") {
                                s->make_term(smt::BVAdd, cnt, s->make_term(1, bvsort4)),
                                s->make_term(0, bvsort4)));
 
+
   // false property
-  smt::Term p = s->make_term(smt::BVUlt, cnt, s->make_term(6, bvsort4));
+  smt::Term p = s->make_term(smt::BVUlt, cnt, s->make_term(5, bvsort4));
 
   cosa::Property prop(fts, p);
-
-  // Note: currently can only use a solver once
-  // e.g. couldn't use in a different engine after this -- would need a new solver
-  // or it might be slow because old assertions will still be there
-  // --> not all solvers support reset_assertions
   cosa::Bmc bmc(prop, s);
 
   cosa::ProverResult pr = bmc.check_until(5);
   CHECK(pr == cosa::FALSE);
 
-
 }
+
+// TEST_CASE("transition_with_datatype") {
+//   smt::SmtSolver s = smt::CVC4SolverFactory::create(false);
+
+//   Theory t=upgradeT(monoid());
+//   Vvvi paths = all_paths(2,2);
+//   CVC::Sort astSort, pathSort, ruleSort;
+//   std::tie (astSort,pathSort,ruleSort) = create_datatypes(s,t,2);
+
+//   s->set_opt("produce-models", "true");
+//   s->set_opt("incremental", "true");
+//   cosa::FunctionalTransitionSystem fts(s);
+// }
